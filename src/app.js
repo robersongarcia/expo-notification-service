82f82f1986490649cd1c24798215cf2604d32542
Roberson Garcia <roberson.gar12@gmail.com>
2023-06-16 05:02:07 -0400

    weekly notifications

--- src/app.js
+++ src/app.js
@@ -2,10 +2,42 @@ const express = require("express")
 const cors = require("cors")
 const logger = require("morgan")
 const { Expo } = require('expo-server-sdk')
-const { getPatientInfo } = require("./helpers/patients")
+const { getPatientInfo, getPatientsTokens } = require("./helpers/patients")
 const { getDoctorsTokens } = require("./helpers/doctors")
 
 let expo = new Expo()
+
+async function sendNotifications(tokens, data, message) {
+  let messages = []
+
+  for (let pushToken of tokens) {
+      //Check that all your push tokens appear to be valid Expo push tokens
+      if (!Expo.isExpoPushToken(pushToken)) {
+        console.error(`Push token ${pushToken} is not a valid Expo push token`)
+        continue
+      }
+    
+      messages.push({
+        to: pushToken,
+        sound: 'default',
+        body: message,
+        data: { data },
+      })
+  }
+
+  let chunks = expo.chunkPushNotifications(messages)
+  let tickets = []
+
+  for (let chunk of chunks) {
+      try {
+        let ticketChunk = await expo.sendPushNotificationsAsync(chunk)
+        console.log(ticketChunk)
+        tickets.push(...ticketChunk)
+      } catch (error) {
+        console.error(error)
+      }
+  }
+}
 
 const app = express()
 
@@ -53,35 +85,25 @@ app.post("/send-notifications", async (req, res) => {
         return res.status(200).send("No tokens found")
     }
 
-    let messages = []
-
-    for (let pushToken of tokens) {
-        //Check that all your push tokens appear to be valid Expo push tokens
-        if (!Expo.isExpoPushToken(pushToken)) {
-          console.error(`Push token ${pushToken} is not a valid Expo push token`);
-          continue;
-        }
-      
-        messages.push({
-          to: pushToken,
-          sound: 'default',
-          body: 'This is a test notification',
-          data: { patientUid: uid},
-        })
-    }
-
-    let chunks = expo.chunkPushNotifications(messages);
-    let tickets = [];
-
-    for (let chunk of chunks) {
-        try {
-          let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-          console.log(ticketChunk);
-          tickets.push(...ticketChunk);
-        } catch (error) {
-          console.error(error);
-        }
-    }
+    await sendNotifications(tokens, uid, 'TestNotification')
 
     res.status(200).send("Notifications sent")
 })
+
+async function sendNotificationsForPatients(data, message) {
+    const tokens = await getPatientsTokens()
+    await sendNotifications(tokens, data, message)
+}
+
+// calculate the number of milliseconds until the next Monday at midnight
+const now = new Date()
+const nextMonday = new Date(now.getFullYear(), now.getMonth(), now.getDate() + (8 - now.getDay()) % 7, 0, 0, 0)
+const timeUntilNextMonday = nextMonday.getTime() - now.getTime()
+
+// run the task once a week (every 604800000 milliseconds)
+setInterval(() => sendNotificationsForPatients( 'data', 'Test Patient notification' ), 604800000, timeUntilNextMonday)
+
+app.get("/send-notifications-for-patients-test", async (req, res) => {
+    await sendNotificationsForPatients( 'data', 'Test Patient notification' )
+    res.status(200).send("Notifications sent")
+} )
--- src/helpers/patients.js
+++ src/helpers/patients.js
@@ -1,5 +1,5 @@
 const {FirebaseDB} = require('../config/firebase')
-const { doc, getDoc } = require("firebase/firestore/lite")
+const { doc, getDoc, query, where, getDocs, collection } = require("firebase/firestore/lite")
 
 const getPatientInfo = async (uid) => {
 
@@ -15,6 +15,28 @@ const getPatientInfo = async (uid) => {
 
 }
 
+const getPatientsTokens = async () => {
+
+    const usersRef = collection(FirebaseDB, "users")
+    const q = query(usersRef, where("userType", "==", "patient"))
+    const usersSnap = await getDocs(q)
+
+    if(usersSnap.empty) {
+        return []
+    }
+
+    const tokens = []
+
+    usersSnap.forEach(doc => {
+        const {token} = doc.data()
+        tokens.push(token)
+    })
+
+    return tokens
+}
+
+
 module.exports = {
-    getPatientInfo
+    getPatientInfo,
+    getPatientsTokens
 }
